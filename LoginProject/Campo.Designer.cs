52fb2ec93ac366e31c419cf8f2ed7ef190c47f32
hstt-coder <eduardolopes19@outlook.com>
2022-06-05 16:22:08 -0300

    aumenta tela

--- LoginProject/Campo.Designer.cs
+++ LoginProject/Campo.Designer.cs
@@ -224,7 +224,7 @@ namespace LoginProject
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1269, 657);
+            this.ClientSize = new System.Drawing.Size(1444, 657);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox2);
